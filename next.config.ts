3709d449bdab1656f49327024a5b8932669024c4
SeptianProject <septianuwoe75@gmail.com>
2026-01-22 17:42:41 +0700

    feat: configure standalone output and set body size limit for server actions

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "2mb",
+    },
+  },
   images: {
     remotePatterns: [
       {
